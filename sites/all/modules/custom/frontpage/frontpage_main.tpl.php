5893656381adb67abf9e573f07d8e3788c23c869
Vicente Baixauli <vibaiher@gmail.com>
2014-10-10 10:16:20 +0200

    Add facebook

--- sites/all/modules/custom/frontpage/frontpage_main.tpl.php
+++ sites/all/modules/custom/frontpage/frontpage_main.tpl.php
@@ -22,11 +22,22 @@
 </div>
 
 <div class='gg-row'>
-  <div class="gg-38pc gg-column">
-    https://www.facebook.com/pages/Deportivo-olimpo-sedavi/286962194763653
-  </div>
+
   <div class="gg-62pc gg-column">
     <a class="twitter-timeline"  href="https://twitter.com/deportivolimpo" data-widget-id="516262401356742656">Tweets por @deportivolimpo</a>
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </div>
+
+  <div class="gg-38pc gg-column">
+    <div class="fb-like-box" data-href="https://www.facebook.com/pages/Deportivo-olimpo-sedavi/286962194763653" data-width="100%" data-height="600" data-colorscheme="light" data-show-faces="false" data-header="true" data-stream="true" data-show-border="false"></div>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&appId=153404114813598&version=v2.0";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+  </div>
+
 </div>
