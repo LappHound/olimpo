e1c7e548e37161a8b3b2b12d65caa7d628fe5757
Vicente <vibaiher@gmail.com>
2014-10-26 18:29:04 +0100

    Add form to update downloads. Add translations

--- sites/all/modules/custom/downloads/download.tpl.php
+++ sites/all/modules/custom/downloads/download.tpl.php
@@ -1,10 +1,10 @@
 <article class="download">
 
     <header>
-        <h2><?= l($download['title'], $download['filepath']); ?></h2>
+        <h2><?= $download['title']; ?></h2>
     </header>
 
     <p><?= $download['description']; ?></p>
 
-    <?= l($download['title'], $download['filepath'], array('attributes' => array('class' => 'download'))); ?>
+    <?= l($download['title'], $download['filepath'], array('attributes' => array('class' => 'download', 'target' => '_blank'))); ?>
 </article>
