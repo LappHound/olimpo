d3f1f7c1b1a3965449b80eb2b2116b8ff5635861
Vicente Baixauli <vibaiher@gmail.com>
2014-10-26 21:25:02 +0100

    Fix node pages

--- sites/all/themes/olimpo/node.tpl.php
+++ sites/all/themes/olimpo/node.tpl.php
@@ -1,14 +1,14 @@
-<div>
+<div class="node node-<?= $node->type; ?>">
   <?php if (isset($banner)) : ?><?= $banner; ?><?php endif; ?>
-  
+
   <? if (!isset($hide_title) || $hide_title === FALSE) : ?>
     <header>
       <h1><?= $node->title; ?></h1><?= isset($twitter) ? "<span class=\"twitter\">$twitter</span>" : ''; ?>
     </header>
   <? endif; ?>
-  
+
   <?= $content; ?>
-  
+
   <? if (isset($disqus_comments)) : ?>
     <?= $disqus_comments; ?>
   <? endif; ?>
