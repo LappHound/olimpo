21cd9f25950bfadaa18bd6a70ca5589229218099
Vicente Baixauli <vibaiher@gmail.com>
2014-12-14 19:14:10 +0100

    Icon first

--- sites/all/modules/custom/downloads/download.tpl.php
+++ sites/all/modules/custom/downloads/download.tpl.php
@@ -1,7 +1,7 @@
 <article class="download">
 
     <header>
-        <h2><?= $download['title']; ?> <?= l('', $download['filepath'], array('attributes' => array('class' => 'download', 'target' => '_blank'))); ?></h2>
+        <h2><?= l('', $download['filepath'], array('attributes' => array('class' => 'download', 'target' => '_blank'))); ?><?= $download['title']; ?></h2>
     </header>
 
     <p><?= $download['description']; ?></p>
