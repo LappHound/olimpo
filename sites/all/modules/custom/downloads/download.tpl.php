e0e1df3944b0abd7a652def673348b5d02566497
Vicente Baixauli <vibaiher@gmail.com>
2014-12-14 19:11:24 +0100

    Fix downloads styles

--- sites/all/modules/custom/downloads/download.tpl.php
+++ sites/all/modules/custom/downloads/download.tpl.php
@@ -1,10 +1,9 @@
 <article class="download">
 
     <header>
-        <h2><?= $download['title']; ?></h2>
+        <h2><?= $download['title']; ?> <?= l('', $download['filepath'], array('attributes' => array('class' => 'download', 'target' => '_blank'))); ?></h2>
     </header>
 
     <p><?= $download['description']; ?></p>
 
-    <?= l($download['title'], $download['filepath'], array('attributes' => array('class' => 'download', 'target' => '_blank'))); ?>
 </article>
